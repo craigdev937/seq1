7dae4a2e3175a73ba044f32d70111a764d20a244
Craig <indigomx9@gmail.com>
2021-07-18 11:38:03 -0400

    Initial setup of the Project.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+(async () => {
+    const app = express();
+
+    app.get("/", (req, res) => res.json({
+        hello: "world!"
+    }));
+
+    const port = process.env.PORT || 9000;
+    app.listen(port, () => {
+        console.log(`Server: http://localhost:${port}`);
+        console.log("Press Ctrl + C to exit.");
+    })
+})();
+
+
+
+
+
